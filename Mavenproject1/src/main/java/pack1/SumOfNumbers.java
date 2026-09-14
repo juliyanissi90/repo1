3018e3f77821ade6c4fc01f92f55f5f4c7c13d92
juro5192 <162127714+juro5192@users.noreply.github.com>
2026-09-14 10:51:38 +0530

    Intial commit

---
+++ Mavenproject1/src/main/java/pack1/SumOfNumbers.java
@@ -0,0 +1,14 @@
+package pack1;
+
+public class SumOfNumbers {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=5;
+		int sum=a+b;
+		System.out.println(sum);
+		// TODO Auto-generated method stub
+
+	}
+
+}
